bc37224d29aa8c85bea680f3a1c98824bc3f2a2c
David <david@trave.io>
2023-08-28 22:40:58 +0100

    Django Admin to manage Location and SignInOutRegister models

--- register_app/admin.py
+++ register_app/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
+from .models import Location, SignInOutRegister
 
 # Register your models here.
+@admin.register(Location)
+class LocationAdmin(admin.ModelAdmin):
+    list_display = ['name', 'address', 'description']
+
+# Register the SignInOutRegister model
+@admin.register(SignInOutRegister)
+class SignInOutRegisterAdmin(admin.ModelAdmin):
+    list_display = ['user', 'location', 'sign_in_time', 'sign_out_time']

8f6fbe5a1526c235970b233a4a0e300f2749a2af
David <david@trave.io>
2023-08-28 22:37:08 +0100

    Created new Django project and register_app

---
+++ register_app/models.py
@@ -0,0 +1,23 @@
+from django.contrib.auth.models import User
+from django.db import models
+# Create your models here.
+
+class Location(models.Model):
+    name = models.CharField(max_length=200)
+    address = models.TextField()
+    description = models.TextField()
+
+    def __str__(self):
+        return self.name
+    
+
+
+class SignInOutRegister(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    location = models.ForeignKey(Location, on_delete=models.CASCADE)
+    sign_in_time = models.DateTimeField()
+    sign_out_time = models.DateTimeField(null=True, blank=True)
+
+    def __str__(self):
+        return f"{self.user.username} - {self.location.name}"
+

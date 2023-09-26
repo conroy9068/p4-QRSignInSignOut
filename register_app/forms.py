5b5e8c3ffcf68b0b0d2c6de87adb748d07c2d051
David <david@trave.io>
2023-09-27 00:21:07 +0100

    add selectLocationSignInOut form

--- register_app/forms.py
+++ register_app/forms.py
@@ -11,3 +11,10 @@ class UserUpdateForm(forms.ModelForm):
     class Meta:
         model = User
         fields = ['username', 'email', 'first_name', 'last_name']
+
+class SelectLocationSignInOut(forms.Form):
+    location = forms.ModelChoiceField(
+        queryset=Location.objects.filter(is_active=True), 
+        label="Select Project",
+        widget=forms.Select(attrs={'class': 'form-select'})
+    )

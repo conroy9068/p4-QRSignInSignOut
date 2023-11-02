aec2f2bc1384e8c59e3aee2b5f1963c8c2cd9baa
David <david@trave.io>
2023-11-02 23:20:31 +0000

    feat: add download_qr path

--- register_app/urls.py
+++ register_app/urls.py
@@ -2,15 +2,11 @@ from django.urls import path
 from . import views 
 
 urlpatterns = [
-    # path('create_location/', views.location_form, name='location_list'),
-    # path('sign_in/<int:location_id>/', views.sign_in, name='sign_in'),
-    # path('sign_out/<int:register_id>/', views.sign_out, name='sign_out'),
-    # path('sign_in/<int:location_id>/', views.sign_in_out_view, name='sign_in_out'),
     path('create_project/', views.create_project, name='create_project'),
-    # path('create_location/', views.create_location, name='create_location'),
     path('no_access/', views.no_access, name='no_access'),
     path('edit_profile/', views.edit_profile, name='edit_profile'),
     path('view_qr_code/<int:location_id>/', views.view_qr_code, name='view_qr_code'),
+    path('download_qr/<int:location_id>/', views.download_qr, name='download_qr'),
     path('admin_dashboard/', views.admin_dashboard, name='admin_dashboard'),
     path('view_profile/', views.view_profile, name='view_profile'),
     path('select_location/', views.select_location_view, name='select_location'),

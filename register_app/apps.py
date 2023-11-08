5e2e615edca542f4e445dc4ee45ca450e5461f3a
David <david@trave.io>
2023-11-08 22:46:29 +0000

    add code doc string and formatting

--- register_app/apps.py
+++ register_app/apps.py
@@ -2,5 +2,11 @@ from django.apps import AppConfig
 
 
 class RegisterAppConfig(AppConfig):
+    """
+    AppConfig for the register_app Django app.
+
+    This AppConfig sets the default_auto_field to "django.db.models.BigAutoField"
+    and the name to "register_app".
+    """
     default_auto_field = "django.db.models.BigAutoField"
     name = "register_app"

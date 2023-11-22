c863c042f3dc4a2b89670b4e67b8254267f09a56
David <david@trave.io>
2023-11-22 15:00:02 +0000

    upd SECRET_KEY to use env variable

--- QRCodeRegister/settings.py
+++ QRCodeRegister/settings.py
@@ -32,7 +32,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-i8)6%3=87x@olqlh@j^tlnj#c*3oaul6m_*5&(fdd&e$^&1o_*'
+SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
